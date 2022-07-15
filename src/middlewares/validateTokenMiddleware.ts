da6a8216b4a68cbed145fb3fa92ebc3f20a17aa2
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-15 00:44:00 -0300

    feat: Register a new credential, on route POST '/credentials'

---
+++ src/middlewares/validateTokenMiddleware.ts
@@ -0,0 +1,35 @@
+﻿import { NextFunction, Request, Response } from "express";
+
+import userRepository from "../repositories/userRepository.js";
+import userUtils from "../utils/userUtils.js";
+
+export async function validateTokenMiddleware(req: Request, res: Response, next: NextFunction) {
+  const { authorization = "" } = req.headers;
+  const token = authorization.replace("Bearer ", "");
+  if (token === "") {
+    throw {
+      name: "unauthorized",
+      message: "⚠ No token provided!",
+    };
+  }
+
+  const { userId } = userUtils.verifyToken(token);
+  if (!userId) {
+    throw {
+      name: "unauthorized",
+      message: "⚠ Invalid token!",
+    };
+  }
+
+  const user = await userRepository.findById(Number(userId));
+  if (!user) {
+    throw {
+      name: "unprocessableEntity",
+      message: "⚠ No user found with given id!",
+    };
+  }
+
+  res.locals.user = user;
+
+  next();
+}
---
+++ src/repositories/credentialRepository.ts
@@ -0,0 +1,14 @@
+﻿import { prisma } from "../config/db.js";
+import { CreateCredentialData } from "../services/credentialService.js";
+
+const credentialRepository = {
+  create: async (createCredentialData: CreateCredentialData) => {
+    await prisma.credentials.create({ data: createCredentialData });
+  },
+
+  getByLabelAndUserId: async (label: string, userId: number) => {
+    return await prisma.credentials.findFirst({ where: { label, userId } });
+  },
+};
+
+export default credentialRepository;
