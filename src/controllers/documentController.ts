d53fc51b679720bec21de903b3c1c4ccab6a0509
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-17 21:41:06 -0300

    feat: Create a new document, on route POST '/documents'

---
+++ src/controllers/documentController.ts
@@ -0,0 +1,14 @@
+﻿import { Request, Response } from "express";
+
+import documentService, { CreateDocumentData } from "../services/documentService.js";
+
+export type CreateDocumentBody = Omit<CreateDocumentData, "userId">;
+
+export async function create(req: Request, res: Response) {
+  const { userId } = res.locals;
+  const createDocumentBody: CreateDocumentBody = req.body;
+
+  await documentService.create({ ...createDocumentBody, userId });
+
+  res.status(201).send("✔ Document created!");
+}
